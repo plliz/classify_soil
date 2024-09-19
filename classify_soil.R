0708d3175107e681092b3656b1235a52d933d590
plliz <153368512+plliz@users.noreply.github.com>
2024-09-18 21:41:38 -0400

    Update classify_soil.R

--- classify_soil.R
+++ classify_soil.R
@@ -8,62 +8,62 @@ classify_soil <- function(sand, silt, clay) {
 	}
 
 	# Sands
-	else if (sand >= 85 && (silt + 1.5 * clay) <= 15) {
+	else if (sand > 85 & (silt + 1.5 * clay) < 15) {
 		return("Sand")
 	}
 
 	# Loamy Sands
-	else if (sand >= 70 && sand < 85 && (silt + 2 * clay) <= 30) {
+	else if (sand >= 70 & sand <= 91 & (silt + 1.5 * clay) >= 15 & (silt + 2 * clay) < 30) {
 		return("Loamy Sand")
 	}
 
 	# Sandy Loams
-	else if (clay <= 20 && sand >= 43 && sand < 85 && (silt + 2 * clay) > 30) {
+	else if ((clay >= 7 & clay <= 20 & sand > 52 & sand < 85 & (silt + 2 * clay) >= 30) | (clay < 7 & silt < 50 & sand > 43)) {
 		return("Sandy Loam")
 	}
 
 	# Loam
-	else if (clay >= 7 && clay <= 27 && silt >= 28 && silt <= 50 && sand < 52) {
+	else if (clay >= 7 & clay <= 27 & silt >= 28 & silt <= 50 & sand <= 52) {
 		return("Loam")
 	}
 
 	# Silt Loam
-	else if ((silt >= 50 && clay >= 12 && clay <= 27) || (silt >= 50 && silt < 80 && clay < 12)) {
-		return("Silt Loam")
+	else if ((silt >= 50 & clay >= 12 & clay <= 27) | (silt >= 50 & silt <= 80 & clay < 12)) {
+		return("Silty Loam")
 	}
 
 	# Silt
-	else if (silt >= 80 && clay < 12) {
+	else if (silt >= 80 & clay < 12) {
 		return("Silt")
 	}
 
 	# Sandy Clay Loam
-	else if (clay >= 20 && clay < 35 && silt < 28 && sand >= 45) {
+	else if (clay >= 20 & clay <= 35 & silt < 28 & sand > 45) {
 		return("Sandy Clay Loam")
 	}
 
 	# Clay Loam
-	else if (clay >= 27 && clay < 40 && sand >= 20 && sand <= 45) {
+	else if (clay >= 27 & clay <= 40 & sand > 20 & sand <= 46) {
 		return("Clay Loam")
 	}
 
 	# Silty Clay Loam
-	else if (clay >= 27 && clay < 40 && sand < 20) {
+	else if (clay >= 27 & clay <= 40 & sand <= 20) {
 		return("Silty Clay Loam")
 	}
 
 	# Sandy Clay
-	else if (clay >= 35 && sand >= 45) {
+	else if (clay >= 35 & sand >= 45) {
 		return("Sandy Clay")
 	}
 
 	# Silty Clay
-	else if (clay >= 40 && silt >= 40) {
+	else if (clay >= 40 & silt >= 40) {
 		return("Silty Clay")
 	}
 
 	# Clay
-	else if (clay >= 40 && sand < 45 && silt < 40) {
+	else if (clay >= 40 & sand <= 45 & silt < 40) {
 		return("Clay")
 	}
 
@@ -73,4 +73,4 @@ classify_soil <- function(sand, silt, clay) {
 }
 
 # Apply the function to your data
-soil$sc <- apply(soil[, c('SAND', 'SILT', 'CLAY')], 1, function(row) classify_soil(row[1], row[2], row[3]))		# Classify soil texture according to USDA classification (c('SAND', 'SILT', 'CLAY'))
+ds1$Texture2 <- apply(ds1[, c('Sand', 'silt', 'Clay')], 1, function(row) classify_soil(row[1], row[2], row[3]))		# Classify soil texture according to USDA (c('SAND', 'SILT', 'CLAY'))
